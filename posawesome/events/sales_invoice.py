4a2baf1439404b374dc3dae72dfb3a9d45525b7e
TaniaNawaz <tanianawaz.7869@gmail.com>
2023-05-30 18:23:53 +0500

    sales invoice (make payment mendatory ):

--- posawesome/events/sales_invoice.py
+++ posawesome/events/sales_invoice.py
@@ -4,17 +4,30 @@ def validate(doc, method=None):
    setting_doc = frappe.get_doc("Pizzaman Setting")
    if setting_doc.include_payment:
       if doc.is_pos:
+          flag_dis = False
+          flag_not = False
+          for row in doc.items:
+            if row.discount_percentage == 100:
+              flag_dis =True
+            else:
+              flag_not =True
+            
           total_paid_amount=0
           for payment in doc.payments:
-              if doc.paid_amount == 0:
-                frappe.throw("Amount can't be zero")
-              else:
-                  total_paid_amount = total_paid_amount + payment.amount
+            total_paid_amount = total_paid_amount + payment.amount
+  
+          doc.db_set('paid_amount',total_paid_amount)
+
+          if total_paid_amount == 0  and flag_dis == False:
+            frappe.throw("Paid Amount can't be zero")
+          elif total_paid_amount == 0  and flag_dis == True and flag_not == True:
+            frappe.throw("Sorry you cannot process further because total discount is not 100%")
+
 
           if doc.grand_total != total_paid_amount:
-              frappe.throw("paid amount is less than grand total")
-   else:
-      frappe.msgprint(f"Please enable the option Include Payment from Doctype Pizzaman Setting")
+              frappe.throw("Sorry you cannot process further because paid amount is not equal to the total amount")
+  #  else:
+  #     frappe.msgprint(f"Please enable the option Include Payment from Doctype Pizzaman Setting")
            
               
 
