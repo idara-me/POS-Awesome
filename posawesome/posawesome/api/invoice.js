b163700910b3da6d80576b6badc1157aa5a25d41
Mian Shahid <mianshahid03206893720@gmail.com>
2023-05-30 14:38:38 +0500

    mode of payment and grand total

--- posawesome/posawesome/api/invoice.js
+++ posawesome/posawesome/api/invoice.js
@@ -25,5 +25,29 @@ frappe.ui.form.on('Sales Invoice', {
             }
         })
     },
+    pos_profile: function (frm) {
+        setTimeout(() => {
+            frm.doc.payments = []
+            frm.refresh_field('payments');
+            let gd_total=0
+            frappe.db.get_doc('POS Profile', null, { 'name': frm.doc.pos_profile })
+            .then(doc => {
+                for(let row of doc.payments){
+                    if(row.default==1 && frm.doc.grand_total > 0){
+                        gd_total=frm.doc.grand_total
+                    }else{
+                        gd_total=0.0
+                    }
+                    frm.add_child('payments', {
+                        default : row.default,
+                        mode_of_payment: row.mode_of_payment,
+                        amount:gd_total
+                    });
+                }
+                frm.refresh_field('payments');
+                })
+            }, 1000);
+
+    }
     
 });
