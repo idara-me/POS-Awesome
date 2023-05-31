6dd5e5f7c3fe47422df06f595ae2e5cba534e73b
Mian Shahid <mianshahid03206893720@gmail.com>
2023-05-31 18:18:30 +0500

    updated(sales invoice)

--- posawesome/posawesome/api/invoice.js
+++ posawesome/posawesome/api/invoice.js
@@ -29,12 +29,12 @@ frappe.ui.form.on('Sales Invoice', {
         setTimeout(() => {
             frm.doc.payments = []
             frm.refresh_field('payments');
-            let gd_total=0
+            let gd_total=0.0
             frappe.db.get_doc('POS Profile', null, { 'name': frm.doc.pos_profile })
             .then(doc => {
                 for(let row of doc.payments){
-                    if(row.default==1 && frm.doc.grand_total > 0){
-                        gd_total=frm.doc.grand_total
+                    if(row.default==1 && frm.doc.rounded_total > 0){
+                        gd_total=frm.doc.rounded_total
                     }else{
                         gd_total=0.0
                     }
@@ -48,6 +48,36 @@ frappe.ui.form.on('Sales Invoice', {
                 })
             }, 1000);
 
+    },
+    customer:function(frm,cdt,cdn){
+        if(frm.doc.customer){
+            frappe.db.get_value('Customer', frm.doc.customer, ['posa_discount'])
+            .then(r => {
+                if(frm.doc.items){
+                    for(let row of frm.doc.items){
+                        frappe.model.set_value(row.doctype,row.name,'discount_percentage',r.message.posa_discount)      
+                    }
+                }
+            })
+        }
     }
     
+});
+
+frappe.ui.form.on('Sales Invoice Item', {
+    item_code:function(frm,cdt,cdn){
+        // var row = locals[cdt][cdn];
+        if(frm.doc.customer){
+            // frappe.msgprint("Hello")
+            frappe.db.get_value('Customer', frm.doc.customer, ['posa_discount'])
+                .then(r => {
+                    console.log(r.message.posa_discount);
+                    console.log(cdt);
+                    console.log(cdn);
+                    setTimeout(function(){ 
+                        frappe.model.set_value(cdt,cdn,'discount_percentage',r.message.posa_discount)
+                    }, 1000);
+                })
+        }
+    }
 });
