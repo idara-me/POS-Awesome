7bca6431b985fc1a1b129db5ba1c03ae6c989d41
TaniaNawaz <tanianawaz.7869@gmail.com>
2023-06-01 11:40:58 +0500

    init : posawesome

--- posawesome/events/sales_invoice.py
+++ posawesome/events/sales_invoice.py
@@ -17,17 +17,11 @@ def validate(doc, method=None):
             total_paid_amount = total_paid_amount + payment.amount
   
           doc.db_set('paid_amount',total_paid_amount)
-
-          if total_paid_amount == 0  and flag_dis == False:
-            frappe.throw("Paid Amount can't be zero")
-          elif total_paid_amount == 0  and flag_dis == True and flag_not == True:
-            frappe.throw("Sorry you cannot process further because total discount is not 100%")
-
+          
 
           if doc.grand_total != total_paid_amount:
               frappe.throw("Sorry you cannot process further because paid amount is not equal to the total amount")
-  #  else:
-  #     frappe.msgprint(f"Please enable the option Include Payment from Doctype Pizzaman Setting")
+
            
               
 
