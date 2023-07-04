0ab64e681b5bfb24a1e2708cf9568b7cb5dd5d27
TaniaNawaz <tanianawaz.7869@gmail.com>
2023-07-04 12:20:07 +0500

    Revert "feat: Initialize App"
    This reverts commit f4420e4ceb655d6fbd294141cb1686ccb347885e.
    Updated

--- posawesome/config/desktop.py
+++ posawesome/config/desktop.py
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from frappe import _
 
 def get_data():
 	return [
 		{
-			"module_name": "Posawesome",
+			"module_name": "POS Awesome",
+			"category": "Modules",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
 			"type": "module",
-			"label": _("Posawesome")
+			"label": _("POS Awesome")
 		}
 	]
--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in posawesome/__init__.py
 from posawesome import __version__ as version
 
 setup(
-	name="posawesome",
+	name='posawesome',
 	version=version,
-	description="POSAWESOME",
-	author="Nextash",
-	author_email="support@nextash.com",
+	description='POS Awesome',
+	author='Youssef Restom',
+	author_email='youssef@totrox.com',
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
